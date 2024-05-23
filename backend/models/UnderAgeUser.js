3bd15d84881e7064cdd606caec0dd799fb06aa08
EnzoDV5 <125486388+EnzoDV08@users.noreply.github.com>
2024-05-23 16:08:03 +0200

    new

--- backend/models/UnderAgeUser.js
+++ backend/models/UnderAgeUser.js
@@ -17,4 +17,4 @@ const UserSchema = new Schema({
     },
 });
 
-module.exports = model('User', UserSchema);
+module.exports = model('UnderAgeUser', UserSchema);
--- rentalfinder/src/pages/home.js
+++ rentalfinder/src/pages/home.js
@@ -1,12 +1,12 @@
 import React from "react";
-import SignIn from './signin';
-// import SignUp from './signup';
+// import SignIn from './signin';
+import SignUp from './signup';
 //import FavoritePage from './favorites'; // Import FavoritePage component
 
 function Home() {
     return (
-      <SignIn />
-      // <SignUp />
+      // <SignIn />
+      <SignUp />
       //<FavoritePage />
     );
   }
