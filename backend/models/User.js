0b78eb89c3127d21b3de51064b21d75078964378
EnzoDV5 <125486388+EnzoDV08@users.noreply.github.com>
2024-05-23 11:59:08 +0200

    mongo db new

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+import { Schema, model } from 'mongoose';
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
     username: {
         type: String,
         required: true,
@@ -16,6 +16,6 @@ const UserSchema = new mongoose.Schema({
     },
 });
 
-module.exports = mongoose.model('User', UserSchema);
+export default model('User', UserSchema);
 
 
