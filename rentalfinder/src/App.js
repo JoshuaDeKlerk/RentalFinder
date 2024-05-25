c512b7cb76a2599b3a1aa278561e5113ba318c04
JoshuaDeKlerk <231207@virtualwindow.co.za>
2024-05-26 00:51:45 +0200

    NavBar Plus Darkmode

--- rentalfinder/src/App.js
+++ rentalfinder/src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import useLocalStorage from "use-local-storage";
+import NavBar from './components/navbar';
 import './App.css';
 import Home from './pages/home';
 import Search from './pages/search';
 import Favorites from './pages/favorites';
 import Booking from './pages/booking';
+import SignIn from './pages/signin';
+import SignUp from './pages/signup';
 
 function App() {
+  const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isDark, setIsDark] = useLocalStorage("isDark", preference);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/booking" element={<Booking />} />
-      </Routes>
-    </Router>
+    <div className='App' data-theme={isDark ? "dark" : "light"}>
+      <NavBar isDark={isDark} setIsDark={setIsDark} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
 export default App;
-
--- rentalfinder/src/pages/home.js
+++ rentalfinder/src/pages/home.js
@@ -1,13 +1,16 @@
 import React from "react";
 // import SignIn from './signin';
-import SignUp from './signup';
+// import SignUp from './signup';
 //import FavoritePage from './favorites'; // Import FavoritePage component
 
 function Home() {
     return (
-      // <SignIn />
-      <SignUp />
-      //<FavoritePage />
+      <div className="Home">
+        {/* // <SignIn />
+        // <SignUp />
+        //<FavoritePage /> */}
+      </div>
+
     );
   }
   
