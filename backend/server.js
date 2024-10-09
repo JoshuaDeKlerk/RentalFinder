7354b354382e6bba1fd49c693bf907d412d2d8fb
Enzo De Vittorio <231244@virtualwindow.co.za>
2024-10-09 18:29:04 +0200

    Updated server to serve rentalfinder frontend

--- backend/server.js
+++ backend/server.js
@@ -57,10 +57,10 @@ app.use('/upload', uploadRoutes);
 
 // Serve the React frontend in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../rentalfinder/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../rentalfinder', 'build', 'index.html'));
   });
 }
 
